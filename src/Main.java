9cad3056e40e748836c2924719ef8527c2a212e6
Adrian R <rod21691@uvg.edu.gt>
2022-03-20 17:52:42 -0600

    Implementacion basica de menu de usuario

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,37 @@
+import java.util.Scanner;
 
 public class Main {
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		boolean quit = false;
+		int option = 0;
 
+		while (!quit) {
+
+			System.out.println("1.Agregar producto a coleccion" + "\n2.Mostrar categoria de producto"
+					+ "\n3.Mostrar coleccion de usuario" + "\n4.Mostrar coleccion de inventario" + "\n5.Salir");
+
+			option = input.nextInt();
+			switch (option) {
+			case 1:
+				System.out.println("Agregar");
+				break;
+			case 2:
+				System.out.println("Mostrar");
+				break;
+			case 3:
+				System.out.println("1. Sin ordenar 2. Ordenada por tipo");
+				break;
+			case 4:
+				System.out.println("1. Sin ordenar 2. Ordenada por tipo");
+				break;
+			case 5:
+				System.out.println("1. Sin ordenar 2. Ordenada por tipo");
+				break;
+
+			}
+
+		}
+
+	}
 }
