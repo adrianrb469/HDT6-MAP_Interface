49593d554565993ab8ace5c33baaaa6a5a72973b
Adrian R <rod21691@uvg.edu.gt>
2022-03-20 20:39:07 -0600

    Finalizacion de clase factory

--- src/MapFactory.java
+++ src/MapFactory.java
@@ -1,5 +1,29 @@
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.TreeMap;
 
 public class MapFactory {
 
-	
+	public MapFactory() {
+		 
+	}
+
+	/***
+	 * This method returns a specific Map implementation
+	 * 0 = Hashmap 1 = LinkedHashMap 2 = TreeMap
+	 * @param map
+	 * @return selected Map implementation
+	 */
+	public Map<String, String[]> getMap(int map){
+		switch (map) {
+			case 0:
+				return new HashMap<String, String[]>();
+			case 1:
+				return new LinkedHashMap<String, String[]>();
+			case 2:
+				return new TreeMap<String, String[]>();
+		}
+		return null;
+	}
 }
